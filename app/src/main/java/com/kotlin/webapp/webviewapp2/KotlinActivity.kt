f4879f623c0ffa9352c159cb28c0543d8feaa24b
nishant <nishjan1996@gmail.com>
2017-12-09 23:01:23 +0530

    Link Correction

--- app/src/main/java/com/kotlin/webapp/webviewapp2/KotlinActivity.kt
+++ app/src/main/java/com/kotlin/webapp/webviewapp2/KotlinActivity.kt
@@ -67,7 +67,7 @@ class KotlinActivity : AppCompatActivity() {
 
     companion object {                                                      // STEP - 01
         private val J_OBJ = "KT"
-        private val INNER_URL = "file:///android_asset/WebViewApp2/webviewapp2.html"
-        private val BASE_URL = "https://fifthsirean02.github.io/webviewapp2.html"
+        private val INNER_URL = "file:///android_asset/WebViewApp2/index.html"
+        private val BASE_URL = "https://fifthsirean02.github.io/webviewpage2/"
     } // End of Companion Object
 }
