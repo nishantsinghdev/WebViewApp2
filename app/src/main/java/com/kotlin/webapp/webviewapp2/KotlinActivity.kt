65c7f6e7e2c222161998973452229689d9dbe3b2
nishant <nishjan1996@gmail.com>
2018-01-17 17:13:51 +0530

    Updated URL

--- app/src/main/java/com/kotlin/webapp/webviewapp2/KotlinActivity.kt
+++ app/src/main/java/com/kotlin/webapp/webviewapp2/KotlinActivity.kt
@@ -87,7 +87,7 @@ class KotlinActivity : AppCompatActivity() {
     companion object {                                                      // STEP - 01
         private val J_OBJ = "KT"
         private val INNER_URL = "file:///android_asset/WebViewApp2/index.html"
-        private val BASE_URL = "https://fifthsirean02.github.io/WebViewApp2/"
+        private val BASE_URL = "https://nishantsinghdev.github.io/WebViewApp2/"
     } // End of Companion Object
 
     private val isNetworkAvailable: Boolean
